bb7f7de57f805d27df7820e43e13789b9d22e534
dhavinaputri <145527688+dhavinaputri@users.noreply.github.com>
2024-08-12 11:53:10 +0700

    Pertemuan7 Mengontrol Aliran Program Pernyataan Switch Case(1)

---
+++ Pertemuan7Tugas1/src/main/java/com/mycompany/pertemuan7tugas1/Pertemuan7Tugas1.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.pertemuan7tugas1;
+
+public class Pertemuan7Tugas1 {
+
+  public static void main (String[] args) {
+    int day = 2;
+    switch (day) {
+      case 1:
+        System.out.println("Sabtu ");
+        break;
+      case 2:
+        System.out.println("Minggu ");
+        break;
+    }
+  }
+}
