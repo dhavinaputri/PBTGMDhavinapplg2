608acec49a2d15d667accef356cacc5d4b482f40
dhavinaputri <145527688+dhavinaputri@users.noreply.github.com>
2024-08-12 11:28:33 +0700

    Pertemuan2 Variabel Pada Java(1)

---
+++ Pertemuan2Tugas1/src/main/java/com/mycompany/pertemuan2tugas1/Pertemuan2Tugas1.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.pertemuan2tugas1;
+
+public class Pertemuan2Tugas1 {
+
+    public static void main(String[] args) {
+        int a = 50;
+        float b = a;
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
