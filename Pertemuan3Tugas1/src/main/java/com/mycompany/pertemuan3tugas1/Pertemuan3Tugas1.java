43bb685bd3bf2d0c72562013985d56c786556057
dhavinaputri <145527688+dhavinaputri@users.noreply.github.com>
2024-08-12 11:34:15 +0700

    Pertemuan3 Tipe Data Pada Java(1)

---
+++ Pertemuan3Tugas1/src/main/java/com/mycompany/pertemuan3tugas1/Pertemuan3Tugas1.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.pertemuan3tugas1;
+
+public class Pertemuan3Tugas1 {
+
+    public static void main(String[] args) {
+   
+    int a = 9;
+    float fl = 8;
+    char ch = 'A';
+    boolean b = false; 
+ 
+ String string = "Hello World";
+ 
+    System.out.println("int Var1 " + a);
+    System.out.println("float Var2 " + fl);
+    System.out.println("char Var3 " + ch);
+    System.out.println("boolean Var4 " + b);
+    System.out.println("string Var5 " + string);
+    
+    }
+}
