d9cc78a5540bec08c661e163a06ee2cb65f77e9e
dhavinaputri <145527688+dhavinaputri@users.noreply.github.com>
2024-08-12 13:03:17 +0700

    Pertemuan9 Perulangan While, Do While(3)

---
+++ Pertemuan9Tugas3/src/main/java/com/mycompany/pertemuan9tugas3/Pertemuan9Tugas3.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.pertemuan9tugas3;
+
+public class Pertemuan9Tugas3 {
+
+    public static void main(String[] args) {
+   int i = 1;
+
+    while (i < 6) {
+      System.out.println(i);
+      i++;
+    }
+  }
+}
