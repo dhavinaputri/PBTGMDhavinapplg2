5c61d5a13c7cf513f01feb40efc2e5a6cfecd70a
dhavinaputri <145527688+dhavinaputri@users.noreply.github.com>
2024-08-12 11:30:35 +0700

    Pertemuan2 Variabel Pada Java(2)

---
+++ Pertemuan2Tugas2/src/main/java/com/mycompany/pertemuan2tugas2/Pertemuan2Tugas2.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.pertemuan2tugas2;
+
+public class Pertemuan2Tugas2 {
+
+public static String Makanan;
+public static final String Minuman = "Jus Jambu";
+public static void main(String[] args) {
+
+            Makanan = "Ayam Geprek ";  //Mengisi Value pada variable Makanan
+                System.out.println("Makanan Favorit Saya " + Makanan);//MemanggilVariable Makanan
+                System.out.println("Minuman Favorit Saya " + Minuman);//Memanggil Variable Minuman
+    }
+}
