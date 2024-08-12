eb92835490284f7cfc03c0811fe5e5904646d700
dhavinaputri <145527688+dhavinaputri@users.noreply.github.com>
2024-08-12 13:02:45 +0700

    Pertemuan9 Perulangan While, Do While(2)

---
+++ Pertemuan9Tugas2/src/main/java/com/mycompany/pertemuan9tugas2/Pertemuan9Tugas2.java
@@ -0,0 +1,26 @@
+
+package com.mycompany.pertemuan9tugas2;
+import java.util.Scanner;
+
+public class Pertemuan9Tugas2 {
+
+    public static void main(String[] args) {
+       Scanner myInput = new Scanner(System.in);
+      
+  System.out.println("Masukan Nilai Bilangan Awal:");
+  int nilaiawal = myInput.nextInt();
+  
+  System.out.println("Masukan Nilai Bilangan Akhir:");
+  int nilaiakhir = myInput.nextInt();
+  
+  System.out.print("Hasil deret bilangan:\n" + nilaiawal );
+      
+      int currentNumber = nilaiawal ;
+      
+      // Menggunakan while loop untuk menampilkan deret bilangan dengan pertambahan 5
+      while (currentNumber + 5 <= nilaiakhir) {
+          currentNumber += 5;
+          System.out.print(", " + currentNumber);
+      }
+  }
+}
