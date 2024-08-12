441d2fa0b3e11e02228911ce1f53e31ea5863387
dhavinaputri <145527688+dhavinaputri@users.noreply.github.com>
2024-08-12 11:35:49 +0700

    Pertemuan3 Tipe Data Pada Java(2)

---
+++ Pertemuan3Tugas2/src/main/java/com/mycompany/pertemuan3tugas2/Pertemuan3Tugas2.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.pertemuan3tugas2;
+
+public class Pertemuan3Tugas2 {
+
+    public static void main(String[] args) {
+        int NIM = 2110117000;
+        String NamaMahasiswa ="Andika Jaya Utama";
+        String kelas = "01SIFE020";
+        double Usia = 19.5;
+        double dl = 1.72;
+        
+        System.out.println("NIM : " + NIM);
+        System.out.println("Nama Mahasiswa : " + NamaMahasiswa);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("Usia : " + Usia);
+        System.out.println("Tinggi : " + dl);
+    }
+}
