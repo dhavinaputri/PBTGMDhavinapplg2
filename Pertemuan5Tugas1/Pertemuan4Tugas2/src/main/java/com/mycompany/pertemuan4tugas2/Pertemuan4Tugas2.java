f1ec62038437223089b405de1dae5f8758098a27
dhavinaputri <145527688+dhavinaputri@users.noreply.github.com>
2024-08-12 11:43:14 +0700

    Pertemuan5 Input Output Stream Dan Keyword Pada Java(1)

---
+++ Pertemuan5Tugas1/Pertemuan4Tugas2/src/main/java/com/mycompany/pertemuan4tugas2/Pertemuan4Tugas2.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.pertemuan4tugas2;
+
+public class Pertemuan4Tugas2 {
+
+    public static void main(String[] args) {
+        
+        int w,x,y,z;
+        
+        x = 6;
+        w = 5;
+        y = 8 - x++;
+        z = 8 - ++w;
+             
+        System.out.println("Nilai x " + x);
+        System.out.println("Nilai w " + w);
+        System.out.println("Nilai y " + y);
+        System.out.println("Nilai z " + z);
+        
+    }
+}
---
+++ Pertemuan5Tugas1/src/main/java/com/mycompany/pertemuan5tugas1/Pertemuan5Tugas1.java
@@ -0,0 +1,36 @@
+
+package com.mycompany.pertemuan5tugas1;
+import java.util.Scanner;
+
+public class Pertemuan5Tugas1 {
+
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+        
+        System.out.println("Masukan Nama Anda");
+        // String input
+        String name = myInput.nextLine();
+        
+        System.out.println("Masukan Alamat Anda");
+        // String input
+        String alamat = myInput.nextLine();
+        
+        // Numerical input
+        System.out.println("Masukan Usia Anda");
+        int usia = myInput.nextInt();
+        
+        // Consume the newline left-over
+        myInput.nextLine();
+        
+        System.out.println("Masukan Gaji Anda");
+        double salary = myInput.nextDouble();
+        
+        // Output input by user
+        System.out.println("### Pendataan Karyawan PT. Petani Kode ###");
+        System.out.println("Nama Karyawan: " + name);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Usia: " + usia);
+        System.out.println("Gaji: " + salary);
+        
+    }
+}
