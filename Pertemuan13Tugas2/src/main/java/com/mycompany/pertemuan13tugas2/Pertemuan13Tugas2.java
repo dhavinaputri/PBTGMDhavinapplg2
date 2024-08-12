7fbcf5ba4e93a6f0e0259dd9e71b2be814a504cb
dhavinaputri <145527688+dhavinaputri@users.noreply.github.com>
2024-08-12 17:22:47 +0700

    Pertemuan13 Polymorphism(1)

---
+++ Pertemuan13Tugas2/src/main/java/com/mycompany/pertemuan13tugas2/Pertemuan13Tugas2.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.pertemuan13tugas2;
+
+public class Pertemuan13Tugas2 {
+
+    public static void main(String[] args) {
+        SegitigaCalculator objek = new SegitigaCalculator();
+        
+        double luas1 = objek.hitungLuas(80, 8);
+        System.out.println("Luas segitiga dengan alas 80 dan tinggi 8 adalah :" + luas1);
+    }
+}
---
+++ Pertemuan13Tugas2/src/main/java/com/mycompany/pertemuan13tugas2/SegitigaCalculator.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.pertemuan13tugas2;
+
+public class SegitigaCalculator {
+    double hitungLuas(double alas, double tinggi) {
+        return (alas * tinggi) / 2;
+    }
+    double hitungLuas(double alas, double tinggi, double luas) {
+        return (alas * tinggi)/2;
+    }
+}
